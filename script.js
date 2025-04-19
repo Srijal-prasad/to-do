fdeed10e8af60f42bf1257986a62c3d33d2d3a7c
Srijal Prasad <srijalprasad@gmail.com>
2025-04-19 21:13:50 +0530

    updating

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const todoForm=document.querySelector(".todo-form");
+    const todoInput=document.querySelector(".todo-input");
+    const todoButton=document.querySelector(".todo-button");
+    const todoList=document.querySelector(".todo-list");
+});
